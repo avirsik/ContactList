75394d9093942cb888ebbfe2edc16a807114f6af
avirsik <annie.virsik@menloschool.org>
2024-01-16 13:23:18 -0800

    Initial Commit

---
+++ src/Student.java
@@ -0,0 +1,9 @@
+public class Student extends Person {
+    private int grade;
+
+    public Student (String firstName, String lastName, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+}
